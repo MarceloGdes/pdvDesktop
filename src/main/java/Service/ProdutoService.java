9be6adde36c6eb4a8fd1be4490848ea3185b8cf7
Rafael Maccari <130612737+rgmaccari@users.noreply.github.com>
2025-06-04 09:21:57 -0300

    creating DTO's and Services

---
+++ src/main/java/Service/ProdutoService.java
@@ -0,0 +1,168 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Service;
+
+import com.google.gson.Gson;
+import dto.ProdutoDto;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import model.Produto;
+
+/**
+ *
+ * @author Rafael
+ */
+public class ProdutoService {
+    private static String BASE_URL = "http://localhost:8080/produtos";
+    private static int SUCESSO = 200;
+    private static int CREATED = 201;
+    
+    public static List<Produto> findAll(String nome) throws Exception{
+        try{
+            String urlString = nome == null || nome.isBlank() ? BASE_URL : BASE_URL + "?nome=" + nome;
+            URL url = new URL(urlString);
+            HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
+            conexao.setRequestMethod("GET");
+            
+            if(conexao.getResponseCode() != SUCESSO){
+                throw new RuntimeException("Erro ao conectar: " + conexao.getResponseMessage());
+            }
+            
+            BufferedReader resposta = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
+            String json = converterJsonString(resposta);
+            Gson gson = new Gson();
+            
+            ProdutoDto[] produtosDto = gson.fromJson(json, ProdutoDto[].class);
+            List<Produto> listaProdutos = Arrays.stream(produtosDto).map(dto -> new Produto(dto)).collect(Collectors.toList());
+            
+            conexao.disconnect();
+            return listaProdutos;
+        }catch(Exception e){
+            throw new Exception("Erro ao consultar produtos: " + e);
+        }
+    }
+    
+    public static Produto insert(ProdutoDto dto) throws Exception{
+        try{
+            URL url = new URL(BASE_URL);
+            HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
+            conexao.setRequestMethod("POST");
+            conexao.setRequestProperty("Content-Type", "application/json");
+            conexao.setDoOutput(true);
+            
+            Gson gson = new Gson();
+            String jsonInput = gson.toJson(dto);
+            
+            try(OutputStream output = conexao.getOutputStream()){
+                output.write(jsonInput.getBytes());
+                output.flush();
+            }
+            
+            if(conexao.getResponseCode() != CREATED){
+                throw new RuntimeException("Erro ao conectar: " + conexao.getResponseMessage());
+            }
+            
+            BufferedReader resposta = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
+            String json = converterJsonString(resposta);
+            ProdutoDto produtoDto = gson.fromJson((json), ProdutoDto.class);
+            Produto produto = new Produto(produtoDto);
+            
+            conexao.disconnect();
+            
+            return produto;
+        }catch(Exception e){
+            throw new Exception("Erro ao inserir produto: " + e);
+        }
+    }
+    
+    public static Produto findById(Integer id) throws Exception{
+        try{
+            URL url = new URL(BASE_URL + "/" + id);
+            HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
+            conexao.setRequestMethod("GET");
+
+            if (conexao.getResponseCode() != SUCESSO) {
+                throw new RuntimeException("Erro ao conectar: " + conexao.getResponseMessage());
+            }
+
+            BufferedReader resposta = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
+            String json = converterJsonString(resposta);
+            Gson gson = new Gson();
+            ProdutoDto produtoDto = gson.fromJson(json, ProdutoDto.class);
+            Produto produto = new Produto(produtoDto);
+            
+            conexao.disconnect();
+            return produto;
+        }catch(Exception e){
+            throw new Exception("Erro ao buscar produto: " + e);
+        }
+    }
+    
+    public static Produto update(Integer id, ProdutoDto dto) throws Exception {
+        try {
+            URL url = new URL(BASE_URL + "/" + id);
+            HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
+            conexao.setRequestMethod("PUT");
+            conexao.setRequestProperty("Content-Type", "application/json");
+            conexao.setDoOutput(true);
+
+            Gson gson = new Gson();
+            String jsonInput = gson.toJson(dto);
+            try (OutputStream os = conexao.getOutputStream()) {
+                os.write(jsonInput.getBytes());
+                os.flush();
+            }
+
+            if (conexao.getResponseCode() != SUCESSO) {
+                throw new RuntimeException("Erro ao conectar: " + conexao.getResponseMessage());
+            }
+
+            BufferedReader resposta = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
+            String json = converterJsonString(resposta);
+            ProdutoDto produtoDto = gson.fromJson(json, ProdutoDto.class);
+            Produto produto = new Produto(produtoDto);
+            
+            conexao.disconnect();
+            return produto;
+
+        } catch (Exception e) {
+            throw new Exception("Erro ao atualizar produto: " + e);
+        }
+    }
+    
+    public static void delete(Integer id) throws Exception {
+        try {
+            URL url = new URL(BASE_URL + "/" + id);
+            HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
+            conexao.setRequestMethod("DELETE");
+
+            if (conexao.getResponseCode() != SUCESSO) {
+                throw new RuntimeException("Erro ao conectar: " + conexao.getResponseMessage());
+            }
+
+            conexao.disconnect();
+
+        } catch (Exception e) {
+            throw new Exception("Erro ao deletar produto: " + e);
+        }
+    }
+    
+    public static String converterJsonString(BufferedReader bufferedReader) throws IOException {
+        StringBuilder jsonString = new StringBuilder();
+        String resposta;
+        while ((resposta = bufferedReader.readLine()) != null) {
+            jsonString.append(resposta);
+        }
+        return jsonString.toString();
+    }
+    
+}
---
+++ src/main/java/dto/ProdutoDto.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dto;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author Rafael
+ */
+public class ProdutoDto {
+    private Integer id;
+    private String descricao;
+    private BigDecimal valor;
+    private String categoria;
+
+    public ProdutoDto(Integer id, String descricao, BigDecimal valor, String categoria) {
+        this.id = id;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.categoria = categoria;
+    }
+
+    public ProdutoDto() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    
+    
+    
+}
